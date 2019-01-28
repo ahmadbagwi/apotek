52a2f9a104db4f107ada528c186baad75fd7f15e
ahmadbagwi <ahmadbagwi.id@gmail.com>
2019-01-29 03:10:07 +0700

    pembersihan file tidak dibutuhkan(model2 pdf yang dipakai tes), program ready

--- application/libraries/Cpdf.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed'); 
-
-Class Cpdf {
- 
-    function __construct() {
-        include_once APPPATH . '/third_party/fpdf/fpdf.php';
-    }
-}
